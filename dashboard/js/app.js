5f924e6a7fc1a8a263cbd2835db11a71cdea67d4
Himanshu Mishra <himanshu.m786@gmail.com>
2016-01-12 20:18:21 +0530

    testing

---
+++ dashboard/js/app.js
@@ -0,0 +1,208 @@
+var teamtaskApp =  angular.module('LoadTeamTask', []);
+
+/* to fill data column 1*/
+teamtaskApp.controller('getstage1Controller', ['$scope', function getstage1Controller($scope){
+	$scope.stage1task = [
+  {
+    "name": "Leah Irwin",
+    "phone": "+1 (862) 582-3244",
+    "email": "Tessa@gmail.com",
+    "city": "Evergreen",
+    "property": "3 BHK"
+  },
+  {
+    "name": "Willie Houston",
+    "phone": "+1 (913) 501-2259",
+    "email": "Jerry@gmail.com",
+    "city": "Juntura",
+    "property": "3 BHK"
+  },
+  {
+    "name": "Margret Ayers",
+    "phone": "+1 (848) 495-2391",
+    "email": "Tania@gmail.com",
+    "city": "Utting",
+    "property": "3 BHK"
+  },
+  {
+    "name": "Martha Nielsen",
+    "phone": "+1 (884) 424-3910",
+    "email": "Cecile@gmail.com",
+    "city": "Longoria",
+    "property": "3 BHK"
+  },
+  {
+    "name": "Carissa Humphrey",
+    "phone": "+1 (831) 487-2864",
+    "email": "Wong@gmail.com",
+    "city": "Fulford",
+    "property": "3 BHK"
+  }
+];
+}]);
+
+/* to fill data in column 2*/
+teamtaskApp.controller('getstage2Controller', ['$scope', function getstage2Controller($scope){
+	$scope.stage2task = [
+  {
+    "name": "Smith Tyson",
+    "phone": "+1 (969) 442-3694",
+    "email": "Hope@gmail.com",
+    "city": "Waukeenah",
+    "property": "3 BHK"
+  },
+  {
+    "name": "Valerie William",
+    "phone": "+1 (805) 545-2347",
+    "email": "Merle@gmail.com",
+    "city": "Blanco",
+    "property": "3 BHK"
+  },
+  {
+    "name": "Patterson Compton",
+    "phone": "+1 (897) 519-2411",
+    "email": "Luz@gmail.com",
+    "city": "Bangor",
+    "property": "3 BHK"
+  },
+  {
+    "name": "Malinda Bass",
+    "phone": "+1 (819) 526-3610",
+    "email": "Isabel@gmail.com",
+    "city": "Guilford",
+    "property": "3 BHK"
+  },
+  {
+    "name": "Dillon Mcbride",
+    "phone": "+1 (808) 510-2264",
+    "email": "Holcomb@gmail.com",
+    "city": "Hamilton",
+    "property": "3 BHK"
+  }
+];
+}]);
+
+
+teamtaskApp.controller('getstage3Controller', ['$scope', function getstage3Controller($scope){
+	$scope.stage3task = [
+  {
+    "name": "Larson Mays",
+    "phone": "+1 (847) 594-3026",
+    "email": "Kristie@gmail.com",
+    "city": "Brandywine",
+    "property": "3 BHK"
+  },
+  {
+    "name": "Janna Odom",
+    "phone": "+1 (808) 576-3016",
+    "email": "Bradford@gmail.com",
+    "city": "Norwood",
+    "property": "3 BHK"
+  },
+  {
+    "name": "Monica Estrada",
+    "phone": "+1 (992) 593-2214",
+    "email": "Marissa@gmail.com",
+    "city": "Virgie",
+    "property": "3 BHK"
+  },
+  {
+    "name": "Carole Figueroa",
+    "phone": "+1 (872) 585-3572",
+    "email": "Santiago@gmail.com",
+    "city": "Newkirk",
+    "property": "3 BHK"
+  },
+  {
+    "name": "Skinner Mccormick",
+    "phone": "+1 (977) 436-3055",
+    "email": "Deanna@gmail.com",
+    "city": "Veguita",
+    "property": "3 BHK"
+  }
+];
+}]);
+
+
+teamtaskApp.controller('getstage4Controller', ['$scope', function getstage4Controller($scope){
+	$scope.stage4task = [
+  {
+    "name": "Rosario Cooley",
+    "phone": "+1 (840) 509-2734",
+    "email": "Rhoda@gmail.com",
+    "city": "Tioga",
+    "property": "3 BHK"
+  },
+  {
+    "name": "Dora Duke",
+    "phone": "+1 (826) 505-2191",
+    "email": "Mcfarland@gmail.com",
+    "city": "Innsbrook",
+    "property": "3 BHK"
+  },
+  {
+    "name": "Hyde Cannon",
+    "phone": "+1 (880) 480-2506",
+    "email": "Angie@gmail.com",
+    "city": "Unionville",
+    "property": "3 BHK"
+  },
+  {
+    "name": "Booth Washington",
+    "phone": "+1 (842) 402-2248",
+    "email": "Adkins@gmail.com",
+    "city": "Carrizo",
+    "property": "3 BHK"
+  },
+  {
+    "name": "Louella Kirkland",
+    "phone": "+1 (850) 415-3525",
+    "email": "Robinson@gmail.com",
+    "city": "Orick",
+    "property": "3 BHK"
+  }
+];
+}]);
+
+
+/* show immdediate task that is need to done */
+teamtaskApp.controller('showMyTask', ['$scope', function showMyTask($scope){
+	$scope.mytask = [
+  {
+    "name": "Rosario Cooley",
+    "phone": "+1 (840) 509-2734",
+    "email": "Rhoda@gmail.com",
+    "city": "Tioga",
+    "property": "3 BHK"
+  },
+  {
+    "name": "Dora Duke",
+    "phone": "+1 (826) 505-2191",
+    "email": "Mcfarland@gmail.com",
+    "city": "Innsbrook",
+    "property": "3 BHK"
+  },
+  {
+    "name": "Hyde Cannon",
+    "phone": "+1 (880) 480-2506",
+    "email": "Angie@gmail.com",
+    "city": "Unionville",
+    "property": "3 BHK"
+  },
+  {
+    "name": "Booth Washington",
+    "phone": "+1 (842) 402-2248",
+    "email": "Adkins@gmail.com",
+    "city": "Carrizo",
+    "property": "3 BHK"
+  },
+  {
+    "name": "Louella Kirkland",
+    "phone": "+1 (850) 415-3525",
+    "email": "Robinson@gmail.com",
+    "city": "Orick",
+    "property": "3 BHK"
+  }
+];
+}]);
+
